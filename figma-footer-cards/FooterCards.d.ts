158216c685709c4881104d5b57b0580d3ae81094
creativex-lab <purvaja@creativex.com>
2026-06-09 16:54:18 -0700

    Add files via upload

---
+++ figma-footer-cards/FooterCards.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+export interface FooterCardsProps {
+  className?: string;
+  style?: React.CSSProperties;
+}
+export declare const FooterCards: React.FC<FooterCardsProps>;
+export default FooterCards;
