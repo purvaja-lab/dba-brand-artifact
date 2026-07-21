b855075212c180513fe8c9af7206f268372dfaa7
creativex-lab <purvaja@creativex.com>
2026-07-21 14:53:21 -0700

    Delete figma-footer-cards directory

--- figma-footer-cards/FooterCards.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-export interface FooterCardsProps {
-  className?: string;
-  style?: React.CSSProperties;
-}
-export declare const FooterCards: React.FC<FooterCardsProps>;
-export default FooterCards;
